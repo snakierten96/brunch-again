4d2885314c6b50c28ad137f42e87ba6e3f93c906
snakierten96 <neil.lunn1@gmail.com>
2017-03-08 08:20:15 +1100

    Initial Dashboard content and styling

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -10,6 +10,14 @@ import { placedOrders } from '../../selectors';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  @select(placedOrders) placedOrders$: Observable<any>;
+
+  tiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' }
+  ];
 
   constructor() { }
 
