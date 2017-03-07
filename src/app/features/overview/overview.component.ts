0a44a1304885eae00e5733e3008d30486a810a93
snakierten96 <neil.lunn1@gmail.com>
2017-03-07 16:54:15 +1100

    Cleanup unused imports

--- src/app/features/overview/overview.component.ts
+++ src/app/features/overview/overview.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { select } from '@angular-redux/store';
-import { IAppState, IParty, ITables, IMenu,
-  rootReducer, middleware, enhancers, reimmutify} from '../../store';
+import { IAppState, IParty, ITables, IMenu } from '../../store';
 import { Observable } from 'rxjs';
 import { LineupActions, TableActions } from '../../actions';
 import { placedOrders } from '../../selectors';
